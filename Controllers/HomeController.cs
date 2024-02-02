53a9e57ff03909f39a2997b6192844b9ab82e3ef
Anjali Bathra <anjalibathra@utexas.edu>
2024-02-01 18:49:04 -0600

    Completed project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Simple_Static_Website.Models;
+
+namespace Simple_Static_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("LoginForm");
+        }
+
+        [HttpGet]
+        public IActionResult LoginForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginForm(LoginViewModel lvm)
+        {
+            if (ModelState.IsValid == false) //repsponse is not valid
+            {
+                return View(lvm);
+            }
+
+            if (lvm.Password != "Bevo" || lvm.Agree != true) //password is incorrect
+            {
+                return View(lvm);
+            }
+
+            //Return the Harry Potter world view
+            return View("Welcome", lvm.House);
+        }
+    }
+}
+
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Simple_Static_Website.Models
+{
+	public enum House { Gryffindor, Hufflepuff, Slytherin, Ravenclaw }
+
+	public class LoginViewModel
+	{
+		[Required(ErrorMessage ="Please select your Hogwarts House.")]
+		public House House { get; set; }
+
+		[Required(ErrorMessage = "Please enter the correct secret phrase to pass through.")]
+		public String Password { get; set; }
+
+		[Required(ErrorMessage = "Please agree to solemnly swear that you are up to no good.")]
+		public Boolean? Agree { get; set; }
+	}
+}
+
